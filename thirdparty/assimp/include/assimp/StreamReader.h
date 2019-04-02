22c70f87540a06f8ecec8b6ad3cab87a61ab5ca7
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2019-04-02 15:14:04 -0700

    Assimp stop shadowing warning.

--- thirdparty/assimp/include/assimp/StreamReader.h
+++ thirdparty/assimp/include/assimp/StreamReader.h
@@ -231,8 +231,8 @@ public:
         return (unsigned int)(current - buffer);
     }
 
-    void SetCurrentPos(size_t pos) {
-        SetPtr(buffer + pos);
+    void SetCurrentPos(size_t _pos) {
+        SetPtr(buffer + _pos);
     }
 
     // ---------------------------------------------------------------------
