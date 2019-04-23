6809f843d9358a6258399cfa677d226c4d33eec7
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2019-04-23 06:21:46 -0700

    Update fetch tbb script.

--- modules/tbb/godot_update_tbb.sh
+++ modules/tbb/godot_update_tbb.sh
@@ -1,4 +1,10 @@
-rm -rf ../../thirdparty/assimp
+#rm -rf ../../thirdparty/tbb
 cd ../../thirdparty/
-git clone https://github.com/01org/tbb.git -b 2019_U5
+#git clone https://github.com/01org/tbb.git -b 2019_U5
 cd tbb
+rm -rf .git
+rm -rf cmake
+rm -rf examples
+rm -rf python
+rm -f .gitignore
+rm -f .gitattributes
