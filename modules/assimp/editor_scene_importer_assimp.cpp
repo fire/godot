b3f6edd2d6526384b6311d459823b7424dc447ec
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2019-07-04 14:12:33 -0700

    Add skeleton to the armature.

--- modules/assimp/editor_scene_importer_assimp.cpp
+++ modules/assimp/editor_scene_importer_assimp.cpp
@@ -393,7 +393,7 @@ Spatial *EditorSceneImporterAssimp::_generate_scene(State &state) {
 			node = node->mParent;
 		}
 		const aiNode *armature_node = node;
-		state.root->add_child(state.skeleton);
+		state.root->find_node(_assimp_string_to_string(armature_node->mName))->add_child(state.skeleton);
 		state.skeleton->set_owner(state.root);
 	}
 	state.skeleton->localize_rests();
