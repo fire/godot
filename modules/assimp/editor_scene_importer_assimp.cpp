03ed5f4cb1dfcd964d105b05950cfb74cb0d5a9a
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2019-07-05 22:01:00 -0700

    Preserve pivots.

--- modules/assimp/editor_scene_importer_assimp.cpp
+++ modules/assimp/editor_scene_importer_assimp.cpp
@@ -113,7 +113,7 @@ Node *EditorSceneImporterAssimp::import_scene(const String &p_path, uint32_t p_f
 	std::string s_path(w_path.begin(), w_path.end());
 	importer.SetPropertyBool(AI_CONFIG_PP_FD_REMOVE, true);
 	// Cannot remove pivot points because the static mesh will be in the wrong place
-	importer.SetPropertyBool(AI_CONFIG_IMPORT_FBX_PRESERVE_PIVOTS, false);
+	importer.SetPropertyBool(AI_CONFIG_IMPORT_FBX_PRESERVE_PIVOTS, true);
 	importer.SetPropertyBool(AI_CONFIG_IMPORT_REMOVE_EMPTY_BONES, false);
 	importer.SetPropertyBool(AI_CONFIG_FBX_CONVERT_TO_M, true);
 	int32_t max_bone_weights = 4;
