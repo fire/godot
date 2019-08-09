8d03b532cfeef2da312526a2b61b3c6e38fe56b0
K. S. Ernest (iFIre) Lee <ernest.lee@chibifire.com>
2019-08-08 23:30:44 -0700

    Make the skeleton root if it's at the root node.

--- modules/assimp/editor_scene_importer_assimp.cpp
+++ modules/assimp/editor_scene_importer_assimp.cpp
@@ -448,7 +448,7 @@ Spatial *EditorSceneImporterAssimp::_generate_scene(State &state) {
 
 		// if the armature is not the root node
 		// todo: find out why we need to do this :(
-		if (node && node != state.scene->mRootNode) {
+		if (node) {
 			// make the skeleton a child of the armature node
 			Node *armature = state.root->find_node(_assimp_get_string(node->mName));
 			ERR_FAIL_COND_V(armature == NULL, state.root);
