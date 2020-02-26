834e3c63a7895f5be21e329926e030096614fa14
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-02-26 10:58:08 -0800

    Rearrange destruction of the managers.

--- modules/persistent_cache/register_types.cpp
+++ modules/persistent_cache/register_types.cpp
@@ -4,21 +4,23 @@
 #include "core/engine.h"
 #include "core/project_settings.h"
 
-static FileCacheManager *file_cache_manager = NULL;
+static Ref<FileCacheManager>file_cache_manager;
 static _FileCacheManager *_file_cache_server = NULL;
 void register_persistent_cache_types() {
-	file_cache_manager = memnew(FileCacheManager);
-	file_cache_manager->init();
-	_file_cache_server = memnew(_FileCacheManager);
 	ClassDB::register_class<FileCache>();
 	ClassDB::register_class<FileCacheFrame>();
 	ClassDB::register_class<FileCacheManager>();
 	ClassDB::register_class<_FileCacheManager>();
 	ClassDB::register_class<_FileAccessCached>();
+	file_cache_manager.instance();
+	file_cache_manager->init();
+	_file_cache_server = memnew(_FileCacheManager);
 	Engine::get_singleton()->add_singleton(Engine::Singleton("FileCacheManager", _FileCacheManager::get_singleton()));
 }
 
 void unregister_persistent_cache_types() {
-	if (file_cache_manager) memdelete(file_cache_manager);
-	if (_file_cache_server) memdelete(_file_cache_server);
+	file_cache_manager.unref();
+	if (_file_cache_server) {
+		memdelete(_file_cache_server);
+	}
 }
