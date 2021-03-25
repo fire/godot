6fbc465487fcdadd8c5150df4920617ce3b7fc54
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-03-25 09:41:13 -0700

    Add changes.

--- modules/etcpak/etcpak_wrap.cpp
+++ modules/etcpak/etcpak_wrap.cpp
@@ -18,12 +18,10 @@ float ecmd_threshold[3] = {0.03f, 0.09f, 0.38f};
 
 void etcpak_wrap_etc2(float p_imgw, float p_imgh, bool p_dither, bool p_etc2, float p_x, float p_y, bool p_mipmap, const uint8_t *p_src, int32_t p_target_size, uint8_t *r_dst) {
 	BlockDataPtr bd = std::make_shared<BlockData>(v2i(p_x, p_y), p_mipmap, p_etc2 == true ? BlockData::Etc2_RGBA : BlockData::Etc1);
-	const int stride = 4;
-	const int block = stride * stride;
 	if (p_etc2) {
-		bd->ProcessRGBA((uint32_t *)p_src, p_imgw / block * p_imgh, 0, p_imgw);
+		bd->ProcessRGBA((uint32_t *)p_src, p_imgw * p_imgh / 16, 0, p_imgw);
 	} else {
-		bd->Process((uint32_t *)p_src, p_imgw / block * p_imgh, 0, p_imgw, Channels::RGB, p_dither);
+		bd->Process((uint32_t *)p_src, p_imgw * p_imgh / 16, 0, p_imgw, Channels::RGB, p_dither);
 	}
 	int wofs = 0;
 	memcpy(r_dst, bd->Decode()->Data(), p_target_size);
@@ -32,9 +30,7 @@ void etcpak_wrap_etc2(float p_imgw, float p_imgh, bool p_dither, bool p_etc2, fl
 
 void etcpak_wrap_bc(float p_imgw, float p_imgh, float p_x, float p_y, bool p_mipmap, const uint8_t *p_src, int32_t p_target_size, uint8_t *r_dst) {
 	BlockDataPtr bd = std::make_shared<BlockData>(v2i(p_x, p_y), p_mipmap, BlockData::Dxt5);
-	const int stride = 4;
-	const int block = stride * stride;
-	bd->ProcessRGBA((uint32_t *)p_src, p_imgw / block * p_imgh, 0, p_imgw);
+	bd->ProcessRGBA((uint32_t *)p_src, p_imgw * p_imgh / 16, 0, p_imgw);
 	int wofs = 0;
 	memcpy(r_dst, bd->Decode()->Data(), p_target_size);
 	bd.reset();
